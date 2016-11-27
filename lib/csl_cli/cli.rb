0382c6bda28393af2407cad9e6417b4b79ab4558
Nils Bartels <nbartels@jimgordon.fritz.box>
2016-11-27 12:33:00 +0100

    [CHANGE] switch creation;

--- lib/csl_cli/cli.rb
+++ lib/csl_cli/cli.rb
@@ -1,5 +1,7 @@
 require 'thor'
 require 'csl_cli'
+require 'json'
+require 'base64'
 
 module CslCli
   class CLI < Thor
@@ -15,14 +17,46 @@ module CslCli
       abort 'app_url missing, please check environment variables' if app_url.nil?
 
       auth = CslCli::Auth.new(app_url)
-      auth.login(email, password)
-      p auth.token
+      logged_in = auth.login(email, password)
+      if logged_in
+        tokenstore = CslCli::Tokenstore.new(ENV['HOME'])
+        tokenstore.store(auth.token)
+        puts "Successfully logged in (token saved to disk)"
+      else
+        abort 'wrong credentials'
+      end
+
 
     end
 
     desc 'switch NOTE', 'creates a context switch with NOTE'
     def switch(note)
+      token = nil
+      decoded_token = {}
 
+      app_url = ENV['CSL_CLI_APP_URL']
+
+      abort 'app_url missing, please check environment variables' if app_url.nil?
+
+      begin
+        tokenstore = CslCli::Tokenstore.new(ENV['HOME'])
+        token = tokenstore.load
+      rescue
+        abort 'Failed loading token from disk. Are you logged in?'
+      end
+
+      decoded_token['header'] = JSON.parse(Base64.decode64(token.split('.')[0]))
+      decoded_token['payload'] = JSON.parse(Base64.decode64(token.split('.')[1]))
+
+      headers = {"Content-Type" => "application/json", "Authorization" => "Bearer #{token}", "Accept" => "application/json"}
+      body = {note: note, user_id: decoded_token['payload']['sub']['$oid']}.to_json
+      request = CslCli::Request::Post.new(app_url + "/switches/", body, headers)
+      if request.code < 300
+        response = request.response
+        puts "Successfully created new context switch: #{response['created_at']}: #{response['id']}: #{response['note']}"
+      else
+        abort "Failed to create new note: #{request.response}"
+      end
     end
 
   end
--- lib/csl_cli/tokenstore.rb
+++ lib/csl_cli/tokenstore.rb
@@ -2,13 +2,27 @@
 
 module CslCli
   class Tokenstore
+    @path = ""
+    @saved_token = nil
 
-    def load
+    attr_reader :path
 
+    def initialize(path)
+      @path = path
     end
 
-    def store()
-      
+    def load
+      file = File.open(File.join(@path, '.csl_token'), 'rb')
+      @saved_token = file.read()
+      file.close
+      return @saved_token
+    end
+
+    def store(token)
+      file = File.open(File.join(@path, '.csl_token'), 'w')
+      file.write(token)
+      file.close
+      return true
     end
 
   end
